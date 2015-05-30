642ea7897a781cecd50326c18643d6ade8f3f13b
오동진 <llioevbee@nate.com>
2015-05-30 17:03:05 +0900

    라이브러리 추가, 인덱스 추가, test class 추가

--- CO_STAR/src/main/java/com/costar/main/DBTest.java
+++ CO_STAR/src/main/java/com/costar/main/DBTest.java
@@ -1,10 +1,33 @@
 package com.costar.main;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class DBTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// commit를 해야 디비에 반영됨
+		try {
+			Reader reader = Resources
+					.getResourceAsReader("config/SqlMapConfig.xml");
 
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder()
+					.build(reader);
+			SqlSession session = factory.openSession();
+		 
+//			session.
+
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
